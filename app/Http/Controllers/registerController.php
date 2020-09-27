de1bb504fac43c5e3ed3323c5e4ff27262ff4647
kudou_kyohei <kudoupg05@gmail.com>
2020-09-27 18:16:31 +0900

    ユーザー名も被らないように追記

--- app/Http/Controllers/registerController.php
+++ app/Http/Controllers/registerController.php
@@ -10,7 +10,7 @@ class RegisterController extends Controller
 {
     public function register(Request $request){
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
