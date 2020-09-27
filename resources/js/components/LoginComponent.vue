5d0b22a9eb73eb5d6061e9a6f14212d56b2e095b
kudou_kyohei <kudoupg05@gmail.com>
2020-09-27 15:26:55 +0900

    ログインフォーム修正

--- resources/js/components/LoginComponent.vue
+++ resources/js/components/LoginComponent.vue
@@ -11,13 +11,7 @@
                                 <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                    <input id="email" type="email" class="form-control" name="email">
                                 </div>
                             </div>
 
@@ -25,13 +19,7 @@
                                 <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                    <input id="password" type="password" class="form-control" name="password">
                                 </div>
                             </div>
 
