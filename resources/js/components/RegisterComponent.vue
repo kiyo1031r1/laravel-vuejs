e730663b305218e825038dbf30715904a0faae2b
kudou_kyohei <kudoupg05@gmail.com>
2020-09-27 15:36:59 +0900

    ユーザー登録ページの作成

---
+++ resources/js/components/RegisterComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">ユーザー作成</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="">
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right">ユーザー名</label>
+
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control" name="text">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password">
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary mr-4">
+                                        作成
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
