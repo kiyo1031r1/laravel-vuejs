c911882d4b42cf50614f888f47574dd4a33697ab
kudou_kyohei <kudoupg05@gmail.com>
2020-09-28 19:01:18 +0900

    バリデーションエラーを投げる処理を追加したつもり

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,17 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
 include 'ChromePhp.php';
 
 class LoginController extends Controller
 {
     public function login(Request $request){
+        // $credentials = $request->validate([
+        //      'email' => ['required', 'email'],
+        //      'password' => ['required']
+        // ]);
+
         if(Auth::attempt([
             'email' => $request->input('email'),
             'password' => $request->input('password'),
         ])){
             return response()->json();
         }
+        throw ValidationException::withMessages([
+            'email' => ['メールアドレスかパスワードが間違っています'],
+            'password' => ['メールアドレスかパスワードが間違っています']
+        ]);
     }
 
     public function logout(){
