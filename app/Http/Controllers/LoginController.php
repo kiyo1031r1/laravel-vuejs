1f0cc978b742793f5fbefd6e583a1b09fa26c082
kudou_kyohei <kudoupg05@gmail.com>
2020-09-28 11:58:08 +0900

    ログイン・ログアウト機能を追加

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function login(){
-        
+    public function login(Request $request){
+
+        if(Auth::attempt([
+            'email' => $request->input['email'],
+            'password' => $request->input['password'],
+        ])){
+            return response();
+        }
+    }
+
+    public function logout(){
+        Auth::logout();
+        return response();
     }
 }
