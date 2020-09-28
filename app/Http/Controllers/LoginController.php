1dc49bb667caa8712b8f465f95d945af2d4c4cac
kudou_kyohei <kudoupg05@gmail.com>
2020-09-28 16:29:33 +0900

    ログを出せるように追加、パラメータ取得のミスを修正

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,23 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use ChromePhp;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+include 'ChromePhp.php';
 
 class LoginController extends Controller
 {
     public function login(Request $request){
-
         if(Auth::attempt([
-            'email' => $request->input['email'],
-            'password' => $request->input['password'],
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
         ])){
+            ChromePhp::log(Auth::check());
+            ChromePhp::log('test2');
             return response();
+        }
+        else{
+            ChromePhp::log('test3');
         }
     }
 
     public function logout(){
         Auth::logout();
+        ChromePhp::log('test');
         return response();
     }
 }
