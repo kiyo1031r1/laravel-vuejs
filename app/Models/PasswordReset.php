cc79f913991f41690da721272d616f03e62149cd
kudou_kyohei <kudoupg05@gmail.com>
2020-09-29 18:43:23 +0900

    テーブルに合わせて、update_atを自動生成しないように変更

--- app/Models/PasswordReset.php
+++ app/Models/PasswordReset.php
@@ -12,4 +12,13 @@ class PasswordReset extends Model
     protected $fillable = [
         'email', 'token'
     ];
+
+    public $timestamps = false;
+
+    public static function boot(){
+        parent::boot();
+        static::creating(function($model) {
+            $model->created_at = $model->freshTimestamp();
+        });
+    }
 }
