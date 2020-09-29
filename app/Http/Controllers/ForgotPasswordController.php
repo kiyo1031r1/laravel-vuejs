469cbe23195c088db691fbb9e60483cb375c83bf
kudou_kyohei <kudoupg05@gmail.com>
2020-09-29 17:37:21 +0900

    トークンの作成

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\support\str;
 
 class ForgotPasswordController extends Controller
 {
@@ -10,6 +11,11 @@ class ForgotPasswordController extends Controller
         $request->validate([
             'email' => ['required', 'email']
         ]);
-        
+
+        $token = $this.createToken();
+    }
+
+    public function createToken(){
+        return hash_hmac('sha256', Str::random(40), config('app.key'));
     }
 }
