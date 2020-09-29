9f4d23e1bf54b4fd1617fa9f6d26f0cddd13b626
kudou_kyohei <kudoupg05@gmail.com>
2020-09-29 16:38:21 +0900

    メール送信後のページを作成

---
+++ resources/js/components/auth/SendMailComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">パスワード再設定用確認メール</div>
+                    <div class="card-body">
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <p>メールを送信しました</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
