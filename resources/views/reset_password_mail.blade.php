b25a568f7cd17f5a4a37ddb85e3324c3a64a9e92
kudou_kyohei <kudoupg05@gmail.com>
2020-10-01 11:30:54 +0900

    メール内容の作成

---
+++ resources/views/reset_password_mail.blade.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>パスワードリセット</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="card">
+                    <div class="card-header">パスワードリセット</div>
+                    <div class="card-body">
+                        <a href="">こちらのリンク</a>をクリックして、パスワードをリセットしてください。
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
