41a75a676ce3bcfd8da2786077bdde172d7e4387
kudou_kyohei <kudoupg05@gmail.com>
2020-10-01 15:47:59 +0900

    コールバックルートを追記

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/reset/{token}', [ResetPasswordController::class, 'reset']);
 
 Route::get('/{any}', function () {
     return view('index');
