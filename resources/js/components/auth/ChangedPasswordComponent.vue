df688efedc323b6a38ce445edf6e778ea1ef0954
kudou_kyohei <kudoupg05@gmail.com>
2020-10-02 11:14:45 +0900

    パスワード変更後のページを作成

---
+++ resources/js/components/auth/ChangedPasswordComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">パスワード再設定</div>
+                    <div class="card-body">
+                        <p class="text-center mb-0 my-4">パスワードを変更しました</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
