4b0b8c5eda709a60bdc4330c7527b8f391d03aa1
kudou_kyohei <kudoupg05@gmail.com>
2020-10-02 12:58:23 +0900

    パスワード再設定時のバリデーション処理を追加

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\PasswordReset;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Validator;
 
 class ResetPasswordController extends Controller
 {
@@ -26,5 +28,38 @@ class ResetPasswordController extends Controller
             $route = "{$baseUrl}/{$this->login_path}";
             return redirect($route)->cookie('ERROR_MESSAGE', $token, 0, '', '', false, false);
         }
+    }
+
+    public function reset(Request $request){
+        $validator = $this->validator($request->all());
+
+        $validator->after(function ($validator, $request) {
+            $token = Crypt::decryptString($request->input('token'));
+
+            $passwordReset = PasswordReset::where('token', $token)->first();
+            if(!$passwordReset){
+                $validator->errors()->add('token', '処理に失敗しました。再度メールを送信してください');
+            }
+
+            $user = User::where('email', $passwordReset->email)->first();
+            if(!$user) {
+                $validator->errors()->add('user', '登録ユーザーが見つかりませんでした');
+            }
+            
+            if($user) {
+                $validator->errors()->add('user', 'テスト');
+            }
+        });
+        
+        $validator->validate();
+
+
+    }
+
+    private function validator($data) {
+        return Validator::make($data, [
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'token' => ['required'] 
+        ]);
     }
 }
