933b4a0389e889b985eb0c6b18ed7535e8bcf5af
kudou_kyohei <kudoupg05@gmail.com>
2020-10-09 18:00:42 +0900

    adminログイン後のページを作成

---
+++ resources/js/components/admin/AdminLoggedINComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">管理者ログイン</div>
+                    <div class="card-body">
+                        <p class="text-center mb-0 my-4">ログインしました</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
