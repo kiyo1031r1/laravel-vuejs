b02e59e41663d17f122fa2016dafc80225c0bdec
kudou_kyohei <kudoupg05@gmail.com>
2020-10-12 16:02:27 +0900

    SNSログイン時の空ページを作成

---
+++ resources/js/components/auth/SNSLoggedInComponent.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+import VueCookies from 'vue-cookies'
+
+export default {
+    created(){
+        if(VueCookies.get('SOCIAL_LOGIN_SUCCESS')) {
+            VueCookies.remove('SOCIAL_LOGIN_SUCCESS');
+            axios.get('/api/user')
+            .then(res => {
+                localStorage.setItem(process.env.MIX_APP_NAME, res.data.token);
+                this.$router.push({name: 'home'});
+            });
+        }
+    }
+}
+</script>
