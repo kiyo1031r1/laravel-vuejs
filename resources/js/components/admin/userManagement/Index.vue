f9f7b060a4d0b2a3f730f9d3a5b4174df82be0b4
kudou_kyohei <kudoupg05@gmail.com>
2020-10-13 14:40:38 +0900

    ユーザー一覧ページを作成

---
+++ resources/js/components/admin/userManagement/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <AdminHeader></AdminHeader>
+        ユーザー一覧
+    </div>
+</template>
+
+<script>
+import AdminHeader from './AdminHeaderComponent'
+
+export default {
+    components:{
+        AdminHeader
+    }
+}
+</script>
