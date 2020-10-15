726b2ab4920c6e7df9d6492b447c17a532b1f898
kudou_kyohei <kudoupg05@gmail.com>
2020-10-15 20:21:49 +0900

    showアクションを追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,4 +10,8 @@ class UserController extends Controller
     public function index(){
         return User::all();
     }
+
+    public function show($id){
+        return User::findOrFail($id);
+    }
 }
