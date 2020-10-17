f357a81c4c9c04825d136d128292bdcb300af188
kudou_kyohei <kudoupg05@gmail.com>
2020-10-17 16:27:42 +0900

    ユーザー検索用に、ダミーデータ数を変更

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -26,6 +26,6 @@ class UserSeeder extends Seeder
             'status' => 'normal'
         ]);
 
-        User::factory()->times(20)->create();
+        User::factory()->times(100)->create();
     }
 }
