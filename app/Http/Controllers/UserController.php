24bee3f89c43194f50d43423aa7af894b395984d
kudou_kyohei <kudoupg05@gmail.com>
2020-10-17 17:40:28 +0900

    userのindexアクションを、検索に対応

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,18 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(){
-        return User::all();
+    public function index(Request $request){
+        $query = User::query();
+        $search = $request->searchWord;
+        if($search) {
+            $search_split = mb_convert_kana($search, 's');
+            $search_split = preg_split('/[\s]+/', $search_split, 0, PREG_SPLIT_NO_EMPTY);
+
+            foreach($search_split as $value) {
+                $query->where('name', 'like', '%'.$value.'%');
+            }
+        }
+        return $query->get();
     }
 
     public function show(User $user){
