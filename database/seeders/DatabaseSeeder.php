02471c27f2fa1739dff188a80dc6aaf9d9acb787
kudou_kyohei <kudoupg05@gmail.com>
2020-10-17 10:45:47 +0900

    DatabaseSeederにUserSeederを追加(TaskSeederはコメントアウト)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(TaskSeeder::class);
+        //$this->call(TaskSeeder::class);
         $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
