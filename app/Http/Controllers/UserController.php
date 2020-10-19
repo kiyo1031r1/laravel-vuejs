0eeb65d4d48591c17465ab6fcad424fc5731a684
kudou_kyohei <kudoupg05@gmail.com>
2020-10-19 11:44:42 +0900

    １ページあたりの表示数を元の値に変更

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index(){
-        return User::paginate(1);
+        return User::paginate(10);
     }
 
     public function show(User $user){
