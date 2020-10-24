26f2ed84074d3bf86895379a0c49518c00169ffd
kudou_kyohei <kudoupg05@gmail.com>
2020-10-24 16:13:40 +0900

    フラッシュメッセージのコンポーネントを作成

---
+++ resources/js/components/FlashMessage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-show="message" class="alert alert-primary" role="alert">
+        {{message}}
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        message(){
+            return this.$store.getters.flashMessage;
+        }
+    }
+}
+</script>
