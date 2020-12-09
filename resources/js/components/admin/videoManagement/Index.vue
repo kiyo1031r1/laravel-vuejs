90c542a8e0efc94466da09fb4b8977ea2f558ff8
kudou_kyohei <kudoupg05@gmail.com>
2020-12-09 11:18:36 +0900

    ビデオ管理画面を作成

---
+++ resources/js/components/admin/videoManagement/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <AdminHeader></AdminHeader>
+        <button class="btn btn-primary" type="submit">ビデオ新規作成</button>
+    </div>
+</template>
+
+<script>
+import AdminHeader from '../AdminHeaderComponent'
+
+export default {
+    components:{
+        AdminHeader
+    }
+}
+</script>
