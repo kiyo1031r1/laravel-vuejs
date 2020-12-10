f8cc616cc2b0acac2acd7b5ef4f6a9a7dc453079
kudou_kyohei <kudoupg05@gmail.com>
2020-12-10 18:14:17 +0900

    videoコントローラーにstoreを仮追加

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Video;
 
 class VideoController extends Controller
 {
-    //
+    public function store(Request $request){
+        return Video::create($request->all());
+    }
 }
