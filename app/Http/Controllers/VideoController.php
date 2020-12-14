988439e9da275043abf68ff34fd342bb6705659e
kudou_kyohei <kudoupg05@gmail.com>
2020-12-14 21:18:36 +0900

    ビデオ情報一覧を取得するアクションを追記(検索こみ)

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -24,4 +24,30 @@ class VideoController extends Controller
 
         return $video;
     }
+
+    public function search(Request $request){
+        $query = Video::query();
+
+        $data = $request->all();
+        $search = $data['search'];
+        $sort = $data['sort'];
+
+        //検索
+        $title = $search['title'];
+        if($title){
+            $this->searchWord($title, 'title', $query);
+        }
+
+        return $query->paginate($sort['per_page']);
+
+    }
+
+    private function searchWord($word, $column, $query){
+        $word_split = mb_convert_kana($word, 's');
+        $word_split = preg_split('/[\s]+/', $word_split, 0, PREG_SPLIT_NO_EMPTY);
+
+        foreach($word_split as $value){
+            $query->where($column, 'like', '%'.$value.'%');
+        }
+    }
 }
