18f5172b6159a308e5078fad6ed12a27e15166e8
kudou_kyohei <kudoupg05@gmail.com>
2020-12-17 11:51:06 +0900

    ページネーションを実装

--- resources/js/components/admin/videoManagement/Index.vue
+++ resources/js/components/admin/videoManagement/Index.vue
@@ -10,6 +10,21 @@
                     </router-link>
                 </div>
             </div>
+            
+            <!-- ページネーション -->
+            <nav>
+                <ul class="pagination justify-content-center">
+                    <li @click="changePage(1)" class="page-item mx-2"><a class="page-link" href="#">先頭</a></li>
+                    <li @click="changePreviousPage()" class="page-item"><a class="page-link" href="#">前</a></li>
+                    <li v-show="leftMorePage" class="mx-2">...</li>
+                    <li v-for="(page, index) in createPageColumn" :key="page.index" @click="changePage(page)" :class="isCurrent(page, index) ? 'page-item active' : 'page-item inactive'">
+                        <a  ref="focus_page" class="page-link" href="#">{{page}}</a>
+                    </li>
+                    <li v-show="rightMorePage" class="mx-2">...</li>
+                    <li @click="changeNextPage()" class="page-item"><a class="page-link" href="#">次</a></li>
+                    <li @click="changePage(last_page)" class="page-item mx-2"><a class="page-link" href="#">最終</a></li>
+                </ul>
+            </nav>
 
             <!-- ビデオ一覧 -->
             <div class="row justify-content-center">
@@ -56,6 +71,10 @@ export default {
             //ページネーション
             current_page: 1,
             last_page: null,
+            page_length: 9,
+            focus_page_index: 0,
+            leftMorePage: false,
+            rightMorePage: false,
 
             //検索
             search:{
@@ -72,6 +91,55 @@ export default {
     },
     components:{
         AdminHeader
+    },
+    computed:{
+        createPageColumn() {
+            const column = [];
+            let start;
+            let last;
+            //指定ページ数以上
+            if(this.last_page > this.page_length){
+                //ページ冒頭処理
+                if(this.current_page <= Math.floor(this.page_length / 2) + 1){
+                    start = 1;
+                    last = this.page_length;
+                    this.leftMorePage = false;
+                    this.rightMorePage = true;
+                }
+                //ページ末尾処理
+                else if(this.current_page >= this.last_page - Math.floor(this.page_length / 2)){
+                    start = this.last_page - this.page_length + 1;
+                    last = this.last_page;
+                    this.leftMorePage = true;
+                    this.rightMorePage = false;
+                }
+                //通常処理
+                else{
+                    start = this.current_page - Math.floor(this.page_length / 2);
+                    last = this.current_page + Math.floor(this.page_length / 2);
+                    this.leftMorePage = true;
+                    this.rightMorePage = true;
+                }
+            }
+            //指定ページ数未満
+            else{
+                start = 1;
+                last = this.last_page;
+                this.leftMorePage = false;
+                this.rightMorePage = false;
+            }
+            
+            for(let i = start; i <= last; i++){
+                column.push(i);
+            }
+
+            //ページ番号へフォーカス
+            if(this.focus_page_index > 0) {
+                this.$refs.focus_page[this.focus_page_index].focus();
+            }
+
+            return column;
+        }
     },
     methods:{
         getVideo(){
@@ -131,7 +199,35 @@ export default {
                     this.getVideo();
                 });
             }
-        }
+        },
+        //ページネーション
+        changeFirstPage(){
+            //最初のユーザーから表示する仕様
+            this.current_page = 1;
+            this.focus_page_index = 0;
+            this.getUser();
+        },
+        changePage(page){
+            this.current_page = page;
+            this.getVideo();
+        },
+        changePreviousPage(){
+            if(this.current_page > 1) {
+                this.changePage(this.current_page - 1);
+            }
+        },
+        changeNextPage(){
+            if(this.current_page < this.last_page) {
+                this.changePage(this.current_page + 1);
+            }
+        },
+        isCurrent(page, index){
+            if(page === this.current_page){
+                this.focus_page_index = index;
+            }
+            return page === this.current_page;
+        },
+        
     },
     created(){
         this.getVideo();
