65051389a9c1f5a22ad369502d9cf826965c48d6
kudou_kyohei <kudoupg05@gmail.com>
2020-12-17 13:48:16 +0900

    サイドバーを実装、それに伴いメイン画面のデザイン変更

--- resources/js/components/admin/videoManagement/Index.vue
+++ resources/js/components/admin/videoManagement/Index.vue
@@ -1,87 +1,131 @@
 <template>
     <div>
         <AdminHeader></AdminHeader>
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="form-inline justify-content-end my-3">
-                        <!-- 表示件数 -->
-                        <label class="col-form-label p-2" for="per_page">表示件数</label>
-                        <select @change="changeFirstPage()" v-model="sort.per_page" class="form-control" id="per_page">
-                            <option value="10">10件</option>
-                            <option value="20">20件</option>
-                            <option value="50">50件</option>
-                            <option value="100">100件</option>
-                        </select>
+            <div class="row">
+                <!-- サイドバー -->
+                <div class="sidebar col-md-2 pr-0">
+                    <!-- ビデオ検索 -->
+                    <p class="font-weight-bold text-center border-bottom border-top py-1 mb-0">ビデオ検索</p>
+                    <div class="px-4">
+                        <form @submit.prevent="changeFirstPage()">
+                            <div class="form-group">
+                                <label class="col-form-label" for="name">ユーザー名</label>
+                                <input v-model="search.name" class="form-control" type="text" id="name">
+                            </div>
 
-                        <!-- 並び替え -->
-                        <label class="col-form-label p-2" for="per_page">並び替え</label>
-                        <select class="form-control">
-                            <option value="10">新しい順</option>
-                            <option value="20">古い順</option>
-                        </select>
+                            <div class="form-group">
+                                <label class="col-form-label">登録日</label>
+                                <Datepicker
+                                    v-model="search.created_at_start"
+                                    :language="datepicker.language"
+                                    :format="datepicker.format"
+                                    :input-class="datepicker.input_class"
+                                    :bootstrap-styling="true"
+                                    :clear-button="true"
+                                    :placeholder="'〜から(未指定可)'"
+                                    :disabled-dates="{from: search.created_at_end}">
+                                </Datepicker>
+                                <Datepicker
+                                    v-model="search.created_at_end"
+                                    :language="datepicker.language"
+                                    :format="datepicker.format"
+                                    :input-class="datepicker.input_class"
+                                    :bootstrap-styling="true"
+                                    :clear-button="true"
+                                    :placeholder="'〜まで(未指定可)'"
+                                    :disabled-dates="{to: search.created_at_start}">
+                                </Datepicker>
+                            </div>
+                            
+                            <div class="col-md-8 mx-auto mt-5 pt-5">
+                                <button class="btn btn-primary btn-block" type="submit">検索</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </div>
 
-            <!-- ビデオ新規作成 -->
-            <div class="row justify-content-center mb-3">
-                <div class="col-md-6">
-                    <router-link :to="{ name: 'video_management_create'}">
-                        <button class="btn btn-primary btn-block">ビデオ新規作成</button>
-                    </router-link>
-                </div>
-            </div>
-            
-            <!-- ページネーション -->
-            <nav>
-                <ul class="pagination justify-content-center">
-                    <li @click="changePage(1)" class="page-item mx-2"><a class="page-link" href="#">先頭</a></li>
-                    <li @click="changePreviousPage()" class="page-item"><a class="page-link" href="#">前</a></li>
-                    <li v-show="leftMorePage" class="mx-2">...</li>
-                    <li v-for="(page, index) in createPageColumn" :key="page.index" @click="changePage(page)" :class="isCurrent(page, index) ? 'page-item active' : 'page-item inactive'">
-                        <a  ref="focus_page" class="page-link" href="#">{{page}}</a>
-                    </li>
-                    <li v-show="rightMorePage" class="mx-2">...</li>
-                    <li @click="changeNextPage()" class="page-item"><a class="page-link" href="#">次</a></li>
-                    <li @click="changePage(last_page)" class="page-item mx-2"><a class="page-link" href="#">最終</a></li>
-                </ul>
-            </nav>
+                <!-- メイン -->
+                <div class="main col-md-9">
+                    <!-- ナビバー -->
+                    <div class="row justify-content-center">
+                        <div class="col-md-8 my-3">
+                            <div class="form-inline justify-content-end">
+                                <!-- 表示件数 -->
+                                <label class="col-form-label p-2" for="per_page">表示件数</label>
+                                <select @change="changeFirstPage()" v-model="sort.per_page" class="form-control" id="per_page">
+                                    <option value="10">10件</option>
+                                    <option value="20">20件</option>
+                                    <option value="50">50件</option>
+                                    <option value="100">100件</option>
+                                </select>
 
-            <!-- ビデオ一覧 -->
-            <div class="row justify-content-center">
-                <div v-for="video in videos" :key="video.id" class="col-md-8 mb-4">
-                    <div class="card">
-                        <div class="row no-gutters">
-                            <div class="col-md-4">
-                                <img class="img-thumbnail" :src="video.thumbnail">
-                            </div>
-                            <div class="col-md-6 border-right">
-                                <div class="card-body">
-                                    <h5 class="card-title mb-1">{{replaceText(video.title, 65)}}</h5>
-                                    <span v-for="category in video.video_category" :key="category.id"
-                                    class="badge badge-secondary mr-1" style="font-size:100%">{{category.name}}
-                                    </span>
-                                    <p class="card-text mt-2">{{replaceText(video.about, 130)}}</p>
-                                </div>
-                            </div>
-                            <div class="col-md-2 my-auto px-2">
-                                <router-link :to="{name: 'video_management_edit', params: { id: video.id}}">
-                                    <button class="btn btn-warning btn-block">編集</button>
+                                <!-- 並び替え -->
+                                <label class="col-form-label p-2" for="per_page">並び替え</label>
+                                <select class="form-control">
+                                    <option value="10">新しい順</option>
+                                    <option value="20">古い順</option>
+                                </select>
+
+                                <!-- ビデオ新規作成 -->
+                                <router-link :to="{ name: 'video_management_create'}">
+                                    <button class="btn btn-primary px-4 ml-4">ビデオ新規作成</button>
                                 </router-link>
-                                <button @click="deleteVideo(video.id)" class="btn btn-danger btn-block mt-2">削除</button>
                             </div>
                         </div>
                     </div>
+
+                    <!-- ビデオ一覧 -->
+                    <div class="row justify-content-center">
+                        <div v-for="video in videos" :key="video.id" class="col-md-8 mb-4">
+                            <div class="card">
+                                <div class="row no-gutters">
+                                    <div class="col-md-4">
+                                        <img class="img-thumbnail" :src="video.thumbnail">
+                                    </div>
+                                    <div class="col-md-6 border-right">
+                                        <div class="card-body">
+                                            <h5 class="card-title mb-1">{{replaceText(video.title, 65)}}</h5>
+                                            <span v-for="category in video.video_category" :key="category.id"
+                                            class="badge badge-secondary mr-1" style="font-size:100%">{{category.name}}
+                                            </span>
+                                            <p class="card-text mt-2">{{replaceText(video.about, 130)}}</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2 my-auto px-2">
+                                        <router-link :to="{name: 'video_management_edit', params: { id: video.id}}">
+                                            <button class="btn btn-warning btn-block">編集</button>
+                                        </router-link>
+                                        <button @click="deleteVideo(video.id)" class="btn btn-danger btn-block mt-2">削除</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- ページネーション -->
+                    <nav>
+                        <ul class="pagination justify-content-center">
+                            <li @click="changePage(1)" class="page-item mx-2"><a class="page-link" href="#">先頭</a></li>
+                            <li @click="changePreviousPage()" class="page-item"><a class="page-link" href="#">前</a></li>
+                            <li v-show="leftMorePage" class="mx-2">...</li>
+                            <li v-for="(page, index) in createPageColumn" :key="page.index" @click="changePage(page)" :class="isCurrent(page, index) ? 'page-item active' : 'page-item inactive'">
+                                <a  ref="focus_page" class="page-link" href="#">{{page}}</a>
+                            </li>
+                            <li v-show="rightMorePage" class="mx-2">...</li>
+                            <li @click="changeNextPage()" class="page-item"><a class="page-link" href="#">次</a></li>
+                            <li @click="changePage(last_page)" class="page-item mx-2"><a class="page-link" href="#">最終</a></li>
+                        </ul>
+                    </nav>
+
                 </div>
             </div>
-            
-        </div>
     </div>
 </template>
 
 <script>
 import AdminHeader from '../AdminHeaderComponent'
+import Datepicker from 'vuejs-datepicker'
+import {ja} from 'vuejs-datepicker/dist/locale'
 
 export default {
     data(){
@@ -100,7 +144,16 @@ export default {
 
             //検索
             search:{
+                name: null,
                 title: null,
+                created_at_start: null,
+                created_at_end: null,
+            },
+
+            datepicker:{
+                language: ja,
+                format: 'yyyy-MM-dd (D)',
+                input_class: 'bg-white mb-2',
             },
 
             //ソート
@@ -112,7 +165,8 @@ export default {
         }
     },
     components:{
-        AdminHeader
+        AdminHeader,
+        Datepicker
     },
     computed:{
         createPageColumn() {
@@ -256,3 +310,17 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.sidebar{
+    border-right: 2px solid #dee2e6;
+}
+.vdp-datepicker >>> .vdp-datepicker__calendar{
+    width: 110%;
+}
+.vdp-datepicker >>> .vdp-datepicker__clear-button{
+    height: calc(1.6em + 0.75rem + 2px);
+    margin-bottom: 0.5rem;
+}
+
+</style>
