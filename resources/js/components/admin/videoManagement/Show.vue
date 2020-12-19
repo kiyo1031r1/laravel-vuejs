4a9539c6e3983a6e900c7a6fad55fcc39b98637a
kudou_kyohei <kudoupg05@gmail.com>
2020-12-19 11:35:24 +0900

    ビデオ視聴ページを作成

---
+++ resources/js/components/admin/videoManagement/Show.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <AdminHeader></AdminHeader>
+        <div class="container">
+        </div>
+    </div>
+</template>
+
+<script>
+import AdminHeader from '../AdminHeaderComponent'
+
+export default {
+    props: ['id']
+    ,
+    components:{
+        AdminHeader
+    }
+}
+</script>
