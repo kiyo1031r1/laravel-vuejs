1e36ad6708a0cf3cd70047e65bb3855270c3c4dc
kudou_kyohei <kudoupg05@gmail.com>
2020-12-19 12:05:18 +0900

    ビデオ視聴ページのファイル名を変更

---
+++ resources/js/components/admin/videoManagement/Watch.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <AdminHeader></AdminHeader>
+        <div class="container">
+        </div>
+    </div>
+</template>
+
+<script>
+import AdminHeader from '../AdminHeaderComponent'
+
+export default {
+    data(){
+        return{
+            video: null,
+        }
+    },
+    props: ['id']
+    ,
+    components:{
+        AdminHeader
+    },
+    methods:{
+        getVideo(){
+            axios.get('/api/videos/watch/' + this.id)
+            .then(res => {
+                this.video = res.data;
+            })
+        }
+    },
+    created(){
+        this.getVideo();
+    }
+}
+</script>
