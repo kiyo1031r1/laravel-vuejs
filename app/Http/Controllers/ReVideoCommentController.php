9a503031c6ee8ecfe4726cdc8ab736f880ae4c19
kudou_kyohei <kudoupg05@gmail.com>
2020-12-20 19:54:09 +0900

    ビデオコメント返信コメントのindexアクションを作成

--- app/Http/Controllers/ReVideoCommentController.php
+++ app/Http/Controllers/ReVideoCommentController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ReVideoComment;
+use App\Models\VideoComment;
 use Illuminate\Http\Request;
 
 class ReVideoCommentController extends Controller
 {
-    //
+    public function index(VideoComment $comment){
+        return ReVideoComment::where('video_comment', $comment->id)->get();
+    }
 }
