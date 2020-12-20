91cb9eab4ff3a940b81d207ad48dd5bfa3eb9a23
kudou_kyohei <kudoupg05@gmail.com>
2020-12-20 19:00:00 +0900

    ビデオコメントにindexアクションを追加

--- app/Http/Controllers/VideoCommentController.php
+++ app/Http/Controllers/VideoCommentController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class VideoCommentController extends Controller
 {
-    //
+    public function index(Video $video){
+        return VideoComment::find($video->id);
+    }
 }
