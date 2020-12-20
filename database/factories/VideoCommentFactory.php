f30b69f431e622bb3e22305722bf704ae63baf70
kudou_kyohei <kudoupg05@gmail.com>
2020-12-20 18:23:43 +0900

    Create VideoCommentFactory.php

---
+++ database/factories/VideoCommentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\VideoComment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class VideoCommentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = VideoComment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'comment' => $this->faker->realText(random_int(0, 200)),
+            'video_id' => random_int(1, 100),
+            'user_id' => random_int(1, 100),
+        ];
+    }
+}
