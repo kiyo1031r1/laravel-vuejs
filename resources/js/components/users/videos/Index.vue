7a2fb97b70208550448968db392bc9d21d75b933
kudou_kyohei <kudoupg05@gmail.com>
2021-01-04 23:33:10 +0900

    カテゴリーアイコンを表示

--- resources/js/components/users/videos/Index.vue
+++ resources/js/components/users/videos/Index.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
         <Header></Header>
+        <div class="container">
+            <div class="row justify-content-center mt-3">
+                <div v-for="icon in icons" :key="icon.index" class="icon col-md-3">
+                    <img :src="'/images/' + icon + '.png'" class="image img-fluid">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,8 +15,26 @@
 import Header from '../UsersHeaderComoponent'
 
 export default {
+    data(){
+        return{
+            icons: ['php','ruby','js','python','csharp','laravel','rails','vue','django'],
+        }
+    },
     components:{
         Header,
     }
 }
 </script>
+
+<style scoped>
+.icon{
+    margin: 20px;
+    padding: 0;
+}
+
+.image{
+    padding: 30px;
+    border: 2px #dee2e6 solid;
+    border-radius: 20px;
+}
+</style>
