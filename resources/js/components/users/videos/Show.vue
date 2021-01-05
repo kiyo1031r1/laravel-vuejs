7361f943c0ab5f74aca929e19f640031ff726e3c
kudou_kyohei <kudoupg05@gmail.com>
2021-01-05 18:15:09 +0900

    カテゴリー別一覧ページで、ビデオを取得(一旦全部)

--- resources/js/components/users/videos/Show.vue
+++ resources/js/components/users/videos/Show.vue
@@ -3,6 +3,7 @@
         <Header></Header>
         <div class="container">
             <div class="row justify-content-center mt-3">
+
             </div>
         </div>
     </div>
@@ -14,12 +15,61 @@ import Header from '../UsersHeaderComoponent'
 export default {
     data(){
         return{
+            //ビデオ
+            videos:[],
+            is_videos: true,
+
+            //ページネーション
+            current_page: 1,
+            last_page: null,
+            page_length: 9,
+            focus_page_index: 0,
+            leftMorePage: false,
+            rightMorePage: false,
+
+            //検索
+            search:{
+                title: '',
+                created_at_start: '',
+                created_at_end: '',
+                categories: [],
+            },
+
+            //ソート
+            sort:{
+                select: 'created_at_desc',
+                per_page: 10,
+            }
         }
     },
-    props: ['id']
+    props: ['category']
     ,
     components:{
         Header,
+    },
+    methods:{
+        getVideo(){
+            //this.search.categories.push(this.category);
+
+            axios.post('/api/videos/search?page=' + this.current_page, {
+                search: this.search,
+                sort: this.sort,
+            })
+            .then(res => {
+                this.videos = res.data.data;
+                if(this.videos.length > 0) {
+                    this.is_videos = true;
+                }
+                else{
+                    this.is_videos = false;
+                }
+
+                this.last_page = res.data.last_page;
+            });
+        }
+    },
+    created(){
+        this.getVideo();
     }
 }
 </script>
