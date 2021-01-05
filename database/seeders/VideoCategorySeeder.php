6d1ef0404de395187ebbd01ce63794b6ea256baf
kudou_kyohei <kudoupg05@gmail.com>
2021-01-05 18:36:58 +0900

    ビデオカテゴリーのダミーデータにファイル名を追加

--- database/seeders/VideoCategorySeeder.php
+++ database/seeders/VideoCategorySeeder.php
@@ -16,30 +16,39 @@ class VideoCategorySeeder extends Seeder
     {
         DB::table('video_categories')->insert([
             'name' => 'PHP',
-        ]);
-        DB::table('video_categories')->insert([
-            'name' => 'JavaScript',
+            'file_name' => 'php',
         ]);
         DB::table('video_categories')->insert([
             'name' => 'Ruby',
+            'file_name' => 'ruby',
+        ]);
+        DB::table('video_categories')->insert([
+            'name' => 'JavaScript',
+            'file_name' => 'javascript',
         ]);
         DB::table('video_categories')->insert([
             'name' => 'Python',
+            'file_name' => 'python',
         ]);
         DB::table('video_categories')->insert([
             'name' => 'C#',
+            'file_name' => 'csharp',
         ]);
         DB::table('video_categories')->insert([
             'name' => 'Laravel',
-        ]);
-        DB::table('video_categories')->insert([
-            'name' => 'VueJs',
+            'file_name' => 'laravel',
         ]);
         DB::table('video_categories')->insert([
             'name' => 'Rails',
+            'file_name' => 'rails',
+        ]);
+        DB::table('video_categories')->insert([
+            'name' => 'VueJs',
+            'file_name' => 'vuejs',
         ]);
         DB::table('video_categories')->insert([
             'name' => 'django',
+            'file_name' => 'django',
         ]);
     }
 }
