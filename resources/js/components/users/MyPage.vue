1abf3f64f3bd70322aefba4b5982b3c1d51d4677
kudou_kyohei <kudoupg05@gmail.com>
2021-01-09 19:27:37 +0900

    マイページのデザイン再調整

--- resources/js/components/users/MyPage.vue
+++ resources/js/components/users/MyPage.vue
@@ -2,39 +2,77 @@
     <div>
         <Header></Header>
         <div class="col-md-6 mx-auto mt-4">
-            <table class="table table-bordered bg-white">
-                <tbody>
-                    <tr class="table-borderless" style="background-color:#00000008;">
-                        <td scope="row">ユーザー情報</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td scope="row">ID</td>
-                        <td>{{user.id}}</td>
-                    </tr>
-                    <tr>
-                        <td scope="row">ユーザー名</td>
-                        <td>{{user.name}}</td>
-                    </tr>
-                    <tr>
-                        <td scope="row">email</td>
-                        <td>{{user.email}}</td>
-                    </tr>
-                    <tr>
-                        <td scope="row">作成日</td>
-                        <td>{{user.created_at | moment_details}}</td>
-                    </tr>
-                    <tr>
-                        <td scope="row">ステータス</td>
-                        <td>{{user.status}}</td>
-                    </tr>
-                    <tr>
-                        <td scope="row">次回更新日</td>
-                        <td v-if="user.next_update != null">{{user.next_update | moment_details}}</td>
-                        <td v-else></td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="card">
+                <div class="card-header">ユーザー情報</div>
+                <div class="card-body">
+                    <!-- ユーザー名 -->
+                    <div class="form-group row">
+                        <label class="col-form-label col-md-3">ユーザー名</label>
+                        <div class="col-md-8">
+                            <input class="form-control" :value="user.name" disabled>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-form-label col-md-3" for="name">ユーザー名を変更</label>
+                        <div class="col-md-8">
+                            <input :class="errors.name ? 'form-control is-invalid' : 'form-control'" id="name" v-model="user.name">
+                            <div v-if="errors.name" class="invalid-feedback">{{ errors.name[0]}}</div>
+                        </div>
+                    </div>
+
+                    <!-- メールアドレス -->
+                    <div class="form-group row pt-5">
+                        <label class="col-form-label col-md-3">メールアドレス</label>
+                        <div class="col-md-8">
+                            <input class="form-control" :value="user.email" disabled>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-form-label col-md-3" for="email">メールアドレスを変更</label>
+                        <div class="col-md-8">
+                            <input :class="errors.email ? 'form-control is-invalid' : 'form-control'" id="email" v-model="user.email">
+                            <div v-if="errors.email" class="invalid-feedback">{{ errors.email[0]}}</div>
+                        </div>
+                    </div>
+
+                    <!-- パスワード -->
+                    <div class="form-group row pt-5">
+                        <label class="col-form-label col-md-3" for="password">パスワードを変更</label>
+                        <div class="col-md-8">
+                            <input type="password" :class="errors.email ? 'form-control is-invalid' : 'form-control'" id="password" v-model="user.password">
+                            <div v-if="errors.password" class="invalid-feedback">{{ errors.password[0]}}</div>
+                        </div>
+                        <div class="password-icon col-md-1"><v-icon name="eye-slash" scale="1.5"/>
+                        </div>
+                    </div>
+
+                    <!-- パスワードの確認 -->
+                    <div class="form-group row">
+                        <label class="col-form-label col-md-3" for="password_confirmation">パスワード再確認</label>
+                        <div class="col-md-8">
+                            <input type="password" class="form-control" id="password_confirmation" v-model="user.password_confirmation">
+                        </div>
+                    </div>
+
+                    <!-- ステータス -->
+                    <div class="form-group row pt-5">
+                        <label class="col-form-label col-md-3">ステータス</label>
+                        <div class="col-md-8">
+                            <input class="form-control" :value="user.status" disabled>
+                        </div>
+                    </div>
+
+                    <!-- 次回更新日 -->
+                    <div class="form-group row pt-5">
+                        <label class="col-form-label col-md-3">次回更新日</label>
+                        <div class="col-md-8">
+                            <input class="form-control" :value="user.next_update === null ? '-' : user.next_update" disabled>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
 
             <div class="col-md-4 mx-auto mt-5">
                 <button @click="edit()" class="btn btn-primary btn-block" type="submit">更新</button>
@@ -52,6 +90,7 @@ export default {
     data(){
         return {
             user:{},
+            errors:{},
         }
     },
     components:{
@@ -79,3 +118,11 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.password-icon {
+    margin: auto;
+    padding: 0;
+    cursor: pointer;
+}
+</style>
