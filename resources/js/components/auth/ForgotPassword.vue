52ebb7ec232c672ea0de1d94463d5da674b312b2
kudou_kyohei <kudoupg05@gmail.com>
2021-01-14 23:05:12 +0900

    ForgotPasswordページの見た目が変わらない程度で、記述を変更

--- resources/js/components/auth/ForgotPassword.vue
+++ resources/js/components/auth/ForgotPassword.vue
@@ -2,36 +2,29 @@
     <div>
         <Header></Header>
         <div class="container">
-            <div class="row justify-content-center mt-3">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">パスワード再設定用確認メール(開発環境のみ可)</div>
-
-                        <div class="card-body">
-                            <form @submit.prevent="submit">
-                                <div class="form-group row">
-                                    <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
-                                    <template v-if="errors.email">
-                                        <div class="col-md-6">
-                                            <input id="email" type="email" class="form-control is-invalid" name="email" v-model="user.email">
-                                            <div class="invalid-feedback">{{ errors.email[0]}}</div>
-                                        </div>
-                                    </template>
-                                    <template v-else>
-                                        <div class="col-md-6">
-                                            <input id="email" type="email" class="form-control" name="email" v-model="user.email">
-                                        </div>
-                                    </template>
+            <div class="col-md-8 mt-3 mx-auto">
+                <div class="card">
+                    <div class="card-header">パスワード再設定用確認メール(開発環境のみ可)</div>
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
+                            <template v-if="errors.email">
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control is-invalid" name="email" v-model="user.email">
+                                    <div class="invalid-feedback">{{ errors.email[0]}}</div>
                                 </div>
-
-                                <div class="form-group row mb-0">
-                                    <div class="col-md-8 offset-md-4">
-                                        <button type="submit" class="btn btn-primary mr-4">
-                                            送信
-                                        </button>
-                                    </div>
+                            </template>
+                            <template v-else>
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email" v-model="user.email">
                                 </div>
-                            </form>
+                            </template>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button @click="submit()" class="btn btn-primary mr-4">送信</button>
+                            </div>
                         </div>
                     </div>
                 </div>
