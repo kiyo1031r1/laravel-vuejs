38c95427e70ed699d4c6641f85076dd52caed748
kudou_kyohei <kudoupg05@gmail.com>
2021-01-14 21:00:49 +0900

    トップの文言変更

--- resources/js/components/users/UserHomeComponent.vue
+++ resources/js/components/users/UserHomeComponent.vue
@@ -5,7 +5,7 @@
             <div class="bg-mask">
                 <div class="title text-center">
                     <h1 class="message">さぁ、動画で</h1>
-                    <h1 class="message">プログラミングを学習だ！</h1>
+                    <h1 class="message">プログラミング学習だ！</h1>
                     <router-link :to="{ name:'login' }">
                         <button class="btn btn-primary btn-lg mt-4">無料会員登録</button>
                     </router-link>
