32d9f62e406f93bf7ff3cc10d7f20df60a3a5579
kudou_kyohei <kudoupg05@gmail.com>
2021-01-14 22:36:10 +0900

    カテゴリーの仕様変更に伴い、ダミーデータを変更

--- database/seeders/VideoVideoCategorySeeder.php
+++ database/seeders/VideoVideoCategorySeeder.php
@@ -20,14 +20,25 @@ class VideoVideoCategorySeeder extends Seeder
         $user_num = 100;
         $video_comment_num = 500;
 
-        for($i = 0; $i < $video_video_category_num; $i++){
+        //ビデオに最低１カテゴリーをつける
+        for($i = 1; $i < $video_num; $i++){
+            $video_category_id = random_int(1, $category_num);
+            DB::table('video_video_category')->insert([
+                'video_id' => $i,
+                'video_category_id' => $video_category_id
+            ]);
+        }
+
+        //2つ目以降のカテゴリーをつける
+        for($j = 0; $j < $video_video_category_num - $video_num; $j++){
             $video_id = random_int(1, $video_num);
             $video_category_id = random_int(1, $category_num);
+
+            //同じカテゴリーがなければ追加
             $video_video_category = DB::table('video_video_category')->where([
                 ['video_id', $video_id],
                 ['video_category_id', $video_category_id]
             ])->get();
-
             if($video_video_category->isEmpty()){
                 DB::table('video_video_category')->insert([
                     'video_id' => $video_id,
