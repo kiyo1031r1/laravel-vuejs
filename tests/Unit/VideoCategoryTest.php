74df67d28524956af286cd6e2a21409feaa68f41
kudou_kyohei <kudoupg05@gmail.com>
2021-02-02 14:52:12 +0900

    videoCategoryモデルの単体テスト

---
+++ tests/Unit/VideoCategoryTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Video;
+use App\Models\VideoCategory;
+use Tests\TestCase;
+
+class VideoCategoryTest extends TestCase
+{
+    public function setUp() :void {
+        parent::setUp();
+        $this->artisan('migrate:fresh');
+        $this->artisan('db:seed');
+    }
+
+    //Roleモデルとのリレーション確認
+    public function testVideoCategoryBelongsToManyVideo(){
+        $count = 5;
+        $video_category = VideoCategory::factory()
+                ->has(Video::factory()->count($count))->create();
+        $this->assertEquals($count, count($video_category->videos));
+    }
+}
