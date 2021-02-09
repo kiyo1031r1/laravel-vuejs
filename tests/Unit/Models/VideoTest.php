de598f6ada1f02951f16597ae7ac9f4d42a1d726
kudou_kyohei <kudoupg05@gmail.com>
2021-02-09 17:20:42 +0900

    コメント消し忘れ

--- tests/Unit/Models/VideoTest.php
+++ tests/Unit/Models/VideoTest.php
@@ -20,7 +20,6 @@ class VideoTest extends TestCase
         $count = 5;
         $video = Video::factory()
                 ->has(VideoCategory::factory()->count($count))->create();
-        //$this->assertEquals($count, count($video->videoCategory));
         $this->assertEquals($count, count($video->videoCategories));
     }
 
