0a496febb22ebb0f2dd38711435aca313a4cc340
kudou_kyohei <kudoupg05@gmail.com>
2021-02-12 17:17:07 +0900

    indexの単体テストを追記

--- tests/Unit/Controllers/VideoCategoryControllerTest.php
+++ tests/Unit/Controllers/VideoCategoryControllerTest.php
@@ -11,4 +11,17 @@ use App\Models\VideoCategory;
 class VideoCategoryControllerTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function testIndex(){
+        $count = 10;
+        VideoCategory::factory()->count($count)->create();
+        $response = $this->getJson('/api/video_categories');
+        $response->assertJsonStructure([
+            '*' => [
+                'id', 'name', 'file_name'
+            ]
+        ])
+        ->assertJsonCount(10)
+        ->assertOk();
+    }
 }
