7e71380567e296c7393ce0ca88d4c70aaa93fd5b
kudou_kyohei <kudoupg05@gmail.com>
2021-02-12 16:59:00 +0900

    Create VideoCategoryControllerTest.php

---
+++ tests/Unit/Controllers/VideoCategoryControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit\Controllers;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Video;
+use App\Models\VideoCategory;
+
+
+class VideoCategoryControllerTest extends TestCase
+{
+    use RefreshDatabase;
+}
