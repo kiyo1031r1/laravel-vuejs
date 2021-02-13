518cdf303e05074b9953e80204ca396942133b6b
kudou_kyohei <kudoupg05@gmail.com>
2021-02-13 20:47:14 +0900

    Create ReVideoCommentControllerTest.php

---
+++ tests/Unit/Controllers/ReVideoCommentControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\ReVideoComment;
+use App\Models\Role;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Video;
+use App\Models\User;
+use App\Models\VideoComment;
+use Carbon\Carbon;
+
+class ReVideoCommentControllerTest extends TestCase
+{
+    use RefreshDatabase;
+}
