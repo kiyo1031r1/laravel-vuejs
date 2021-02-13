2fa15e180ceb8a454e2f52a416a2ffcf2c83f14e
kudou_kyohei <kudoupg05@gmail.com>
2021-02-13 20:43:52 +0900

    getCommentアクションの単体テストを追記

--- tests/Unit/Controllers/VideoCommentControllerTest.php
+++ tests/Unit/Controllers/VideoCommentControllerTest.php
@@ -2,12 +2,14 @@
 
 namespace Tests\Unit\Controllers;
 
+use App\Models\ReVideoComment;
 use App\Models\Role;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Video;
 use App\Models\User;
 use App\Models\VideoComment;
+use Carbon\Carbon;
 
 class VideoCommentControllerTest extends TestCase
 {
@@ -49,5 +51,63 @@ class VideoCommentControllerTest extends TestCase
             'comment_string' => [11111111, 422, 'comment', 'コメントには文字列を指定してください。'],
             'comment_max' => [str_repeat('a', 256), 422, 'comment', 'コメントには255文字以下の文字列を指定してください。'],
         ];
+    }
+
+    public function testGetComment(){
+        $user = User::factory()->for(Role::factory())->create();
+
+        $video1 = Video::factory()->create();
+        $comment_new = VideoComment::factory()->for($user)->for($video1)
+        ->create([
+            'created_at' => Carbon::now()
+        ]);
+
+        $comment_old = VideoComment::factory()->for($user)->for($video1)
+        ->create([
+            'created_at' => Carbon::now()->subDay(1)
+        ]);
+        ReVideoComment::factory()->for($user)->for($comment_new)->create();
+        
+        //ダミーデータ
+        $video2 = Video::factory()->create();
+        $comment2 = VideoComment::factory()->for($user)->for($video2)->create();
+        ReVideoComment::factory()->for($user)->for($comment2)->create();
+
+        $response = $this->postJson('/api/video_comments/get_comment'.'?page='.'1', [
+            'video_id' => $video1->id,
+            'per_page' =>  '20',
+        ]);
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id', 
+                    'comment', 
+                    'video_id', 
+                    'user_id', 
+                    'created_at', 
+                    'updated_at',
+                    'user' => [
+                        'id', 'name'
+                    ],
+                    're_video_comments' => [
+                        '*' => [
+                            'id', 
+                            're_comment', 
+                            'video_comment_id', 
+                            'user_id', 
+                            'created_at', 
+                            'updated_at',
+                            'user' => [
+                                'id', 'name'
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ])
+        ->assertJsonCount(2, 'data')
+        ->assertOk();
+        $this->assertGreaterThan($response['data'][0]['id'], $response['data'][1]['id']);
     }
 }
