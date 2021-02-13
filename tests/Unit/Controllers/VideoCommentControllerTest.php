fe4c8aa838d835850ccec243cdb5b1da3203d4ee
kudou_kyohei <kudoupg05@gmail.com>
2021-02-13 15:21:52 +0900

    Create VideoCommentControllerTest.php

---
+++ tests/Unit/Controllers/VideoCommentControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Unit\Controllers;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Video;
+
+class VideoCommentControllerTest extends TestCase
+{
+    use RefreshDatabase;
+}
