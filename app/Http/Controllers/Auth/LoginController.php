3b1ef5ba24ecc12ad93542f383b5c16fbe5fb2ea
kudou_kyohei <kudoupg05@gmail.com>
2021-02-14 11:50:47 +0900

    このバリデーションは不要なので削除

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -17,11 +17,7 @@ class LoginController extends Controller
     private $sns_login_path = 'sns_login';
 
     public function login(Request $request){
-        $credentials = $request->validate([
-             'email' => ['required', 'email'],
-             'password' => ['required']
-        ]);
-
+        $credentials = $request->only('email', 'password');
         if(Auth::attempt($credentials)){
             $request->session()->regenerate();
         }
