8db96923def0f8006a26ae087317a383762a8dd5
kudou_kyohei <kudoupg05@gmail.com>
2021-02-17 16:39:04 +0900

    ヘッダーのログイン・ログアウトボタンにクラス名を追記

--- resources/js/components/users/Header.vue
+++ resources/js/components/users/Header.vue
@@ -22,12 +22,12 @@
 
                     <template v-if="!isAuthenticated">
                         <router-link :to="{ name: 'login'}">
-                            <button class="btn btn-success">ログイン</button>
+                            <button class="btn btn-success login">ログイン</button>
                         </router-link>
                     </template>
 
                     <template v-else>
-                        <button @click="logout" class="btn btn-success">ログアウト</button>
+                        <button @click="logout" class="btn btn-success logout">ログアウト</button>
                     </template>
                 </div>
             </nav>
