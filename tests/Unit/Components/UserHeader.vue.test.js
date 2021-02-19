82efdcc8f7999ef655f8741f3074e8e34d0dd88f
kudou_kyohei <kudoupg05@gmail.com>
2021-02-19 11:19:18 +0900

    ユーザーのHeader.vueの単体テストを追加

--- tests/Unit/Components/UserHeader.vue.test.js
+++ tests/Unit/Components/UserHeader.vue.test.js
@@ -1,16 +1,27 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
-import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import UserHeader from '@/components/users/Header'
+import flushPromises from 'flush-promises'
 
 const localVue = createLocalVue()
-localVue.use(VueRouter)
 localVue.use(Vuex)
 
+let mockError = false
+jest.mock("axios", () => ({
+    get: () => new Promise((resolve, reject) => {
+        if(mockError){
+            reject()
+        }
+        else{
+            resolve()
+        }
+    }),
+    post: () => Promise.resolve(),
+}))
+
 describe('UserHeader', () => {
-    let state
-    let getters
-    let store
+    let state, getters, store
+    const mockRouterPush = jest.fn();
 
     beforeEach(() => {
         state = {
@@ -27,28 +38,67 @@ describe('UserHeader', () => {
         })
     })
     
-    it('ログイン時は、ログインユーザー名が表示されている', () => {
-        const wrapper = shallowMount(UserHeader, {store, localVue})
+    it('display login user name if login', () => {
+        const wrapper = shallowMount(UserHeader, { store, stubs: ['router-link'], localVue})
         expect(wrapper.find('span.name').text()).toBe(getters.user(state).name + 'さん')
     })
 
-    it('非ログイン時は、ユーザー名が表示されない', () => {
+    it('not display login user name if logout', () => {
         state.user = null
-        const wrapper = shallowMount(UserHeader, {store, localVue})
+        const wrapper = shallowMount(UserHeader, {store, stubs: ['router-link'], localVue})
         expect(wrapper.find('span.name').exists()).toBe(false)
     })
 
-    it('ログイン時は、ログアウトボタンが表示されている', () => {
-        const wrapper = shallowMount(UserHeader, {store, localVue})
+    it('display logout button if login, not display login button', () => {
+        const wrapper = shallowMount(UserHeader, {store, stubs: ['router-link'], localVue})
         expect(wrapper.find('button.login').exists()).toBe(false);
         expect(wrapper.find('button.logout').exists()).toBe(true);
     })
 
-    it('非ログイン時は、ログインボタンが表示されている', () => {
+    it('display login button if logout, not display logout button', () => {
         state.user = null
-        const wrapper = shallowMount(UserHeader, {store, localVue})
+        const wrapper = shallowMount(UserHeader, {store, stubs: ['router-link'], localVue})
         expect(wrapper.find('button.login').exists()).toBe(true);
         expect(wrapper.find('button.logout').exists()).toBe(false);
+    })
+
+    it('testLogout', async () => {
+        const wrapper = shallowMount(UserHeader, { 
+            mocks: { 
+                    $router : {
+                        push : mockRouterPush
+                    }
+            },
+            store,
+            stubs: ['router-link'], 
+            localVue
+        })
+
+        wrapper.find('button.logout').trigger('click')
+        await flushPromises()
+        expect(mockRouterPush).toHaveBeenCalledWith({
+          name: 'login'
+        })
+    })
+
+    it('testLogout_if_auto_logout', async () => {
+        mockError = true
+        const wrapper = shallowMount(UserHeader, { 
+            mocks: { 
+                    $router : {
+                        push : mockRouterPush
+                    }
+            },
+            store,
+            stubs: ['router-link'], 
+            localVue
+        })
+
+        wrapper.find('button.logout').trigger('click')
+        await flushPromises()
+        expect(mockRouterPush).toHaveBeenCalledWith({
+          name: 'login'
+        })
     })
 
 })
