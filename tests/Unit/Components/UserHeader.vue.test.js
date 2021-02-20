5993f81084a240d1aa88b68075645cf17b361a07
kudou_kyohei <kudoupg05@gmail.com>
2021-02-20 11:58:44 +0900

    リファクタリング、及びitを日本語で記述

--- tests/Unit/Components/UserHeader.vue.test.js
+++ tests/Unit/Components/UserHeader.vue.test.js
@@ -39,43 +39,50 @@ describe('UserHeader', () => {
             getters
         })
     })
+
+    const factory = () => {
+        return shallowMount(UserHeader, {
+            mocks: { 
+                $router : {
+                    push : mockRouterPush
+                }
+            },
+            store,
+            stubs : ['router-link'],
+            localVue,
+        })
+    }
+
+    afterEach(() => {
+        mockRouterPush.mockClear()
+    })
     
-    it('display login user name if login', () => {
-        const wrapper = shallowMount(UserHeader, { store, stubs: ['router-link'], localVue})
+    it('ログイン時に、ログインユーザー名が表示されていること', () => {
+        const wrapper = factory()
         expect(wrapper.find('span.name').text()).toBe(getters.user(state).name + 'さん')
     })
 
-    it('not display login user name if logout', () => {
+    it('非ログイン時に、ログインユーザー名が表示されていないこと', () => {
         state.user = null
-        const wrapper = shallowMount(UserHeader, {store, stubs: ['router-link'], localVue})
+        const wrapper = factory()
         expect(wrapper.find('span.name').exists()).toBe(false)
     })
 
-    it('display logout button if login, not display login button', () => {
-        const wrapper = shallowMount(UserHeader, {store, stubs: ['router-link'], localVue})
-        expect(wrapper.find(sel('login')).exists()).toBe(false);
+    it('ログイン時に、ログアウトボタンが表示され、ログインボタンが表示されないこと', () => {
+        const wrapper = factory()
         expect(wrapper.find(sel('logout')).exists()).toBe(true);
+        expect(wrapper.find(sel('login')).exists()).toBe(false);
     })
 
-    it('display login button if logout, not display logout button', () => {
+    it('非ログイン時に、ログインボタンが表示され、ログアウトボタンが表示されないこと', () => {
         state.user = null
-        const wrapper = shallowMount(UserHeader, {store, stubs: ['router-link'], localVue})
+        const wrapper = factory()
         expect(wrapper.find(sel('login')).exists()).toBe(true);
         expect(wrapper.find(sel('logout')).exists()).toBe(false);
     })
 
-    it('testLogout', async () => {
-        const wrapper = shallowMount(UserHeader, { 
-            mocks: { 
-                    $router : {
-                        push : mockRouterPush
-                    }
-            },
-            store,
-            stubs: ['router-link'], 
-            localVue
-        })
-
+    it('logoutのテスト', async () => {
+        const wrapper = factory()
         wrapper.find(sel('logout')).trigger('click')
         await flushPromises()
         expect(mockRouterPush).toHaveBeenCalledWith({
@@ -83,19 +90,9 @@ describe('UserHeader', () => {
         })
     })
 
-    it('testLogout_if_auto_logout', async () => {
+    it('logoutのテスト(自動ログアウト時)', async () => {
         mockError = true
-        const wrapper = shallowMount(UserHeader, { 
-            mocks: { 
-                    $router : {
-                        push : mockRouterPush
-                    }
-            },
-            store,
-            stubs: ['router-link'], 
-            localVue
-        })
-
+        const wrapper = factory()
         wrapper.find(sel('logout')).trigger('click')
         await flushPromises()
         expect(mockRouterPush).toHaveBeenCalledWith({
