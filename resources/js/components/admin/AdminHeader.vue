7ed516858d66091dcb5bd2dd065c44e31b73ce7a
kudou_kyohei <kudoupg05@gmail.com>
2021-03-02 17:22:42 +0900

    画面サイズによって、管理者のヘッダーメニューの表示方法を切り替え

--- resources/js/components/admin/AdminHeader.vue
+++ resources/js/components/admin/AdminHeader.vue
@@ -3,20 +3,45 @@
         <div class="container">
             <nav class="navbar">
                 <router-link :to="{ name: 'home'}">
-                    <span class="h1 text-light">Laravel-vuejs</span>
+                    <span class="h1 logo text-light">どうぷろ！</span>
                 </router-link>
                     <span class="name">管理者 : {{isAuthenticated.name}}さん</span>
-                <div>
+
+                <!-- 画面サイズが大きければ通常変更 -->
+                <div v-if="width > 800">
                     <router-link :to="{ name: 'user_management'}">
-                        <button class="btn btn-success">ユーザー管理</button>
+                        <a>ユーザー管理</a>
                     </router-link>
 
                     <router-link :to="{ name: 'video_management'}">
-                        <button class="btn btn-success">ビデオ管理</button>
+                        <a>ビデオ管理</a>
                     </router-link>
                     
-                    <button @click="logout" class="btn btn-success">ログアウト</button>
+                    <router-link :to="{ name: ''}">
+                        <a @click="logout" >ログアウト</a>
+                    </router-link>
                 </div>
+
+                <!-- 画面サイズが小さければドロップダウンで表示 -->
+                <div v-else class="btn-group" role="group">
+                    <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Menu
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                        <router-link :to="{ name: 'user_management'}">
+                            <a class="dropdown-item">ユーザー管理</a>
+                        </router-link>
+
+                        <router-link :to="{ name: 'video_management'}">
+                            <a class="dropdown-item">ビデオ管理</a>
+                        </router-link>
+                        
+                        <router-link :to="{ name: ''}">
+                            <a class="dropdown-item" @click="logout" >ログアウト</a>
+                        </router-link>
+                    </div>
+                </div>
+
             </nav>
         </div>
     </div>
@@ -24,6 +49,11 @@
 
 <script>
 export default {
+    data() {
+        return {
+            width: window.innerWidth,
+        }
+    },
     computed:{
         isAuthenticated(){
             return this.$store.getters.user;
@@ -42,16 +72,56 @@ export default {
             .catch(() => {
                 this.$router.push({name: 'admin_login'});
             });
+        },
+        handleResize(){
+            this.width = window.innerWidth;
         }
+    },
+    mounted(){
+        window.addEventListener('resize', this.handleResize);
+    },
+    beforeDestroy(){
+        window.removeEventListener('resize', this.handleResize);
     }
 }
 </script>
 
 <style scoped>
+.logo {
+    font-family: "ヒラギノ角ゴ Std W8",'Hiragino Kaku Gothic Std'
+}
+
 .name{
     color: white;
     font-size: 16px;
     margin-right: 5px;
     vertical-align: bottom;
 }
+
+a{
+    padding: 0 5px;
+    color: white;
+    text-decoration: none;
+}
+
+a:hover {
+    color: silver;
+}
+
+a:visited {
+    color: inherit;
+}
+
+.dropdown-menu.show {
+    background-color: #212529;
+}
+
+.dropdown-item {
+    padding: 0 10px;
+}
+
+a.dropdown-item:hover {
+    background-color: inherit;
+}
+
 </style>
