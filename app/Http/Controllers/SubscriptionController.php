a1dc95c502572ba56395e5f9754856eb88ef0683
kudou_kyohei <kudoupg05@gmail.com>
2021-03-13 19:23:50 +0900

    インテントを返す処理をindexに記載

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SubscriptionController extends Controller
 {
     public function index(){
+        $user = Auth::user();
+        return ['intent' => $user->createSetupIntent()];
     }
 }
