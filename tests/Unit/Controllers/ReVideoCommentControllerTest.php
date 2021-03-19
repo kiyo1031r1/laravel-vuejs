ef1b92a144bb3e871eef54ead4fc41f949b23b83
kudou_kyohei <kudoupg05@gmail.com>
2021-03-19 21:31:52 +0900

    パラメータ名変更

--- tests/Unit/Controllers/ReVideoCommentControllerTest.php
+++ tests/Unit/Controllers/ReVideoCommentControllerTest.php
@@ -27,7 +27,7 @@ class ReVideoCommentControllerTest extends TestCase
         $video_comment = VideoComment::factory()->for($video)->for($user)->create();
 
         $response = $this->postJson('/api/re_video_comments', [
-            're_comment' => $request,
+            're_video_comment' => $request,
             'video_comment_id'=> $video_comment->id,
             'user_id'=> $user->id,
         ]);
@@ -38,7 +38,7 @@ class ReVideoCommentControllerTest extends TestCase
         elseif($code === 200){
             $response->assertStatus($code);
             $this->assertDatabaseHas('re_video_comments', [
-                're_comment' => $request,
+                're_video_comment' => $request,
                 'video_comment_id'=> $video_comment->id,
                 'user_id'=> $user->id,
             ]);
@@ -48,9 +48,9 @@ class ReVideoCommentControllerTest extends TestCase
     public function storeDataProvider(){
         return[
             'pass' => [str_repeat('a', 255), 200, null, null],
-            're_comment_required' => [' ', 422, 're_comment', 'コメントは必ず入力してください。'],
-            're_comment_string' => [11111111, 422, 're_comment', 'コメントには文字列を指定してください。'],
-            're_comment_max' => [str_repeat('a', 256), 422, 're_comment', 'コメントには255文字以下の文字列を指定してください。'],
+            're_video_comment_required' => [' ', 422, 're_video_comment', 'コメントは必ず入力してください。'],
+            're_video_comment_string' => [11111111, 422, 're_video_comment', 'コメントには文字列を指定してください。'],
+            're_video_comment_max' => [str_repeat('a', 256), 422, 're_video_comment', 'コメントには255文字以下の文字列を指定してください。'],
         ];
     }
 
