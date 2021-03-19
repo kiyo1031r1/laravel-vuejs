e9bb760f31389d5f657da4ab51cd345028b4a283
kudou_kyohei <kudoupg05@gmail.com>
2021-03-19 21:48:47 +0900

    ReVideoCommentのupdateアクションのテストを追加

--- tests/Unit/Controllers/ReVideoCommentControllerTest.php
+++ tests/Unit/Controllers/ReVideoCommentControllerTest.php
@@ -54,6 +54,44 @@ class ReVideoCommentControllerTest extends TestCase
         ];
     }
 
+    /**
+     * @dataProvider updateDataProvider
+     * @param パラメータ
+     * @param コード
+     * @param カラム名
+     * @param エラーメッセージ
+     */
+    public function testUpdate($request, $code, $column, $message){
+        $video = Video::factory()->create();
+        $user = User::factory()->for(Role::factory())->create();
+        $video_comment = VideoComment::factory()->for($video)->for($user)->create();
+        $re_video_comment = ReVideoComment::factory()->for($video_comment)->for($user)->create();
+
+        $response = $this->putJson('/api/re_video_comments/'.$re_video_comment->id, [
+            'edit_re_video_comment' => $request,
+        ]);
+
+        if($code === 422){
+            $response->assertJsonValidationErrors([$column => $message])
+            ->assertStatus($code);
+        }
+        elseif($code === 200){
+            $response->assertStatus($code);
+            $this->assertDatabaseHas('re_video_comments', [
+                're_video_comment' => $request,
+            ]);
+        }
+    }
+
+    public function updateDataProvider(){
+        return[
+            'pass' => [str_repeat('a', 255), 200, null, null],
+            'edit_re_video_comment_required' => [' ', 422, 'edit_re_video_comment', 'コメントは必ず入力してください。'],
+            'edit_re_video_comment_string' => [11111111, 422, 'edit_re_video_comment', 'コメントには文字列を指定してください。'],
+            'edit_re_video_comment_max' => [str_repeat('a', 256), 422, 'edit_re_video_comment', 'コメントには255文字以下の文字列を指定してください。'],
+        ];
+    }
+
     public function testDestroy(){
         $user = User::factory()->for(Role::factory())->create();
         $video = Video::factory()->create();
