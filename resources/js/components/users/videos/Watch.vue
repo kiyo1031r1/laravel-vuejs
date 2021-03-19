0ddf1ce5e8da050dd204d9c4dc0b405575355174
kudou_kyohei <kudoupg05@gmail.com>
2021-03-19 17:50:42 +0900

    初期化メソッドに、返信コメント返信の項目を追加

--- resources/js/components/users/videos/Watch.vue
+++ resources/js/components/users/videos/Watch.vue
@@ -462,8 +462,10 @@ export default {
             this.my_comment = '';
             this.my_re_comment = '';
             this.my_edit_comment = '';
+            this.my_edit_re_comment = '';
             this.is_re_comment_form = false;
             this.is_edit_comment_form = false;
+            this.is_edit_re_comment_form = false;
             this.comments = [];
             this.getComment(null, this.start_page, false);
         },
