9e1f689edf8d31316c7d11c56064ef4aa81ae358
kudou_kyohei <kudoupg05@gmail.com>
2021-03-22 07:55:51 +0900

    videoとvideoEvaluationのリレーションを記述

--- app/Models/VideoEvaluation.php
+++ app/Models/VideoEvaluation.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class VideoEvaluation extends Model
 {
     use HasFactory;
+
+    public function video(){
+        return $this->belongsTo(Video::class);
+    }
 }
