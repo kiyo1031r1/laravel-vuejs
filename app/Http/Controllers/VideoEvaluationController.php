55399eec5d4a701e0639c0a7d4ab3ef9f3478c03
kudou_kyohei <kudoupg05@gmail.com>
2021-03-22 11:36:29 +0900

    evaluationアクションに、video情報を元に、平均評価値を返す処理を記述

--- app/Http/Controllers/VideoEvaluationController.php
+++ app/Http/Controllers/VideoEvaluationController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\VideoEvaluation;
+use App\Models\Video;
 
 class VideoEvaluationController extends Controller
 {
-    //
+    public function evaluation(Video $video){
+        $evaluations = VideoEvaluation::find($video)->all();
+        if(count($evaluations) > 0){
+            $sum = 0;
+            foreach($evaluations as $evaluate){
+                $sum += $evaluate->evaluation;
+            }
+            $evaluation = $sum / count($evaluations);
+        }
+        else{
+            $evaluation = 0;
+        }
+
+        return $evaluation;
+    }
 }
